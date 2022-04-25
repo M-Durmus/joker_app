fca6127babe0cee1fd13b55ff0177a85df42ebb3
M-Durmus <mdurmus9114@gmail.comm>
2022-04-25 18:52:56 +0300

    first commit

---
+++ src/components/styled/Wrapper.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { DefaultTheme} from "styled-components";
+
+const Wrapper = styled.div`
+display: flex;
+align-items: center
+max-width : 80rem;
+padding: 20px ;
+margin: 0auto;
+`;
+export default Wrapper
---
+++ src/my-theme.ts
@@ -0,0 +1,15 @@
+
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: '#051937',
+    secondary: '#A8EB12',
+    white : "#fff",
+    purple : "#53239C"
+  },
+};
+
+export { myTheme };
